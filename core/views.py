9ac7e8abe27d1088001e3771305f7f1884d97a7d
paudelarpan <paudelarpan91@gmail.com>
2024-05-13 18:02:18 +0545

    initial commit

---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+    return render(request , 'index.html')    
+
+def signup(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        print(username)
+    else:
+        return render(request, 'signup.html')
+    
